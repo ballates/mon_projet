a3fd1a53130be202d6cc88da2a9bd6bfc9752d9f
Ben Mbairo <mbairo.allatessem@gmail.com>
2025-01-31 09:41:03 +0100

    Add my bash script

---
+++ script_b.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Afficher un message de bienvenue
+echo "Bonjour!"
+
+# Afficher le nom d'utilisateur actuel
+echo "Votre nom d'utilisateur est: $USER"
+
+# Lister les fichiers du répertoire courant
+ls -l
